cecc0cad3f7b2300f1287be65a4d175b54f20e1d
ZenoRauch <zeno@rauch-nauer.ch>
2025-11-02 15:15:17 +0100

    updated hashing in pwd

--- backend/app/password.py
+++ backend/app/password.py
@@ -1,10 +1,11 @@
+# password.py
 from passlib.context import CryptContext
 
+# pbkdf2_sha256 funktioniert ohne externe Dependencies
+pwd_context = CryptContext(schemes=['pbkdf2_sha256'], deprecated='auto')
 
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-
-def verify_password(plain_pwd, hash_pwd):
+def verify_password(plain_pwd: str, hash_pwd: str) -> bool:
     return pwd_context.verify(plain_pwd, hash_pwd)
 
-def get_pwd_hash(pwd):
+def get_pwd_hash(pwd: str) -> str:
     return pwd_context.hash(pwd)
