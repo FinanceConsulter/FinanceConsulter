8ad5d3e218f6a7bddf61d2ddb19a17d379c3ae3b
ZenoRauch <zeno@rauch-nauer.ch>
2025-11-07 14:51:33 +0100

    codereview, altered way of handing over Session, used pydantic dump, altered function update behaviour

--- backend/app/repository/account.py
+++ backend/app/repository/account.py
@@ -3,7 +3,10 @@ from models.user import User
 from models.account import Account
 from schemas.account import AccountCreate, AccountUpdate, AccountResponse
 
-class RepositoryAccount:
+class AccountRepository:
+    def __init__(self, db:Session):
+        self.db = db
+        
     @staticmethod
     def convert_to_response(list:list[Account]):
         newList = []
@@ -11,50 +14,52 @@ class RepositoryAccount:
             newList.append(item.to_response())
         return newList
     
-    def get_userspecific_accounts(self, db:Session, current_user: User):
-        accounts = db.query(Account).filter(Account.user_id == current_user.id).all()
+    def get_userspecific_accounts(self, current_user: User):
+        accounts = self.db.query(Account).filter(
+            Account.user_id == current_user.id
+        ).all()
         return self.convert_to_response(accounts)
     
-    def get_account(self, db:Session, current_user: User, account_id: int):
-        account = db.query(Account).filter(Account.user_id == current_user.id, Account.id == account_id).first()
+    def get_account(self, current_user: User, account_id: int):
+        account = self.db.query(Account).filter(
+            Account.user_id == current_user.id, 
+            Account.id == account_id
+        ).first()
         if account == None:
             return None
         return account.to_response()
  
-    def create_account(self, db:Session, current_user: User, account: AccountCreate):
-        if self.check_existing_account(db,current_user, account.name):
+    def create_account(self, current_user: User, account: AccountCreate):
+        if self.check_existing_account(current_user, account.name):
             return None
         new_account = Account(
             user_id = current_user.id,
-            name = account.name,
-            type = account.type,
-            currency_code = account.currency_code
+            **account.model_dump()
         )
-        db.add(new_account)
-        db.commit()
-        db.refresh(new_account)
+        self.db.add(new_account)
+        self.db.commit()
+        self.db.refresh(new_account)
         return new_account.to_response()
     
-    def update_account(self, db:Session, current_user: User, updated_account: AccountUpdate):
-        if self.check_existing_account(db, current_user, updated_account.name):
+    def update_account(self, current_user: User, updated_account: AccountUpdate):
+        if self.check_existing_account(current_user, updated_account.name):
             return None
-        account = db.query(Account).filter(
+        account = self.db.query(Account).filter(
             Account.id == updated_account.id,
             Account.user_id == current_user.id
         ).first()
-        if not updated_account.name == "":
-            account.name = updated_account.name
-        if not updated_account.type == "":
-            account.type = updated_account.type
-        if not updated_account.currency_code == "":
-            account.currency_code = updated_account.currency_code
-        db.add(account)
-        db.commit()
-        db.refresh(account)
+        
+        update_data = updated_account.model_dump(exclude_none=True)
+        for field, value in update_data.items():
+            if field != 'id':
+                setattr(account, field, value)
+            
+        self.db.commit()
+        self.db.refresh(account)
         return account.to_response()
     
-    def check_existing_account(self, db:Session, current_user: User, account_name: str):
-        existing_account = db.query(Account).filter(
+    def check_existing_account(self, current_user: User, account_name: str):
+        existing_account = self.db.query(Account).filter(
             Account.name == account_name, 
             Account.user_id == current_user.id).first()
         if existing_account:
