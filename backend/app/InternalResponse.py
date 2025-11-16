f10c3fd93881d050abd89508e476963aa6212de7
BastiBuilds <bastian.bueeler@gmail.com>
2025-11-16 17:18:27 +0100

    correct state assignment in InternalResponse class from tuple to one state

--- backend/app/InternalResponse.py
+++ backend/app/InternalResponse.py
@@ -1,5 +1,5 @@
 from fastapi import status
 class InternalResponse:
     def __init__(self, state: status,detail: str):
-        self.state = state, 
+        self.state = state
         self.detail = detail
